b4583a4b9bc91f56c8580985fa4c46d72278ffe0
email <155702916+SouravInnoraft@users.noreply.github.com>
2024-02-29 16:05:58 +0000

    FT2-77: Resolved Errors and Updated File Structure.

--- PHP basics task 1-6 /output.php
+++ PHP basics task 1-6 /output.php
@@ -44,12 +44,12 @@ if (isset($_POST['submit']) && isset($_FILES['image']) &&
   $target_file = './images/' . $_FILES['image']['name'];
   // If image upload is successful display the image else display an error message.
   if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
-  ?>
+?>
     <img src=<?= $target_file ?> alt='#'>
 <?php
   }
   else {
-  ?>
+?>
     <p>Not uploaded successfully</p>
 <?php
   }
